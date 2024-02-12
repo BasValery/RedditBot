beb131448791c9cc93c2bbeaa005a6832dfd7330
Valerii Bas <valerii.bas@avenga.com>
2024-02-12 22:24:53 +0200

    Add project files.

---
+++ RedditBot/ImageBotService.cs
@@ -0,0 +1,47 @@
+﻿
+
+using TL;
+
+namespace RedditBot
+{
+    public class ImageBotService
+    {
+        private readonly TelegramConfig telegramConfig;
+        public ImageBotService(TelegramConfig telegramConfig)
+        {
+           this.telegramConfig = telegramConfig ?? throw new ArgumentNullException(nameof(telegramConfig));
+        }
+
+        public async Task Run()
+        {
+            using var client = new WTelegram.Client(Config);
+
+            Console.WriteLine("Connecting to Telegram...");
+            await client.LoginUserIfNeeded();
+
+            var channelName = "moarlurk"; 
+            Console.WriteLine($"Accessing the channel: {channelName}");
+
+            var channel = await client.Contacts_ResolveUsername(channelName);
+            var peer = new InputPeerChannel(channel.Channel.ID, channel.Channel.access_hash);
+
+            Console.WriteLine("Getting messages...");
+            var messages = await client.Messages_GetHistory(peer, limit: 10);
+            foreach (var message in messages.Messages)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        private string Config(string what)
+        {
+            switch (what)
+            {
+                case "api_id": return telegramConfig.ApiId; 
+                case "api_hash": return telegramConfig.ApiHash;
+                case "phone_number": return telegramConfig.PhoneNumber;
+                default: return null;
+            }
+        }
+    }
+}
---
+++ RedditBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using RedditBot;
+
+class Program
+{
+
+    private static IConfigurationRoot? Configuration;
+    static async Task Main(string[] args)
+    {
+        Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+        var telegramConfig = Configuration.GetSection("Telegram").Get<TelegramConfig>();
+
+        if(telegramConfig == null)
+        {
+            Console.WriteLine("Telegram configuration was not loaded"); 
+            return;
+        }
+
+        var imageBotService = new ImageBotService(telegramConfig);
+        await imageBotService.Run();
+    }
+}
