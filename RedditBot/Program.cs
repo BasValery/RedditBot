90cb22eec60156d4494256c1adcdf926eecaf18e
Valerii Bas <valerii.bas@avenga.com>
2024-02-16 22:02:13 +0200

    Add settings to .gitignore

--- RedditBot/Program.cs
+++ RedditBot/Program.cs
@@ -3,20 +3,26 @@ using RedditBot;
 
 class Program
 {
-
     private static IConfigurationRoot? Configuration;
+
     static async Task Main(string[] args)
     {
+        // Retrieve the environment name from the environment variables (default to "Production" if not set)
+        var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
         Configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true)
-                .Build();
+            .SetBasePath(Directory.GetCurrentDirectory())
+            // Load the base configuration file
+            .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true)
+            // Load the environment-specific configuration file (e.g., "AppSettings.Development.json")
+            .AddJsonFile($"AppSettings.{environmentName}.json", optional: true, reloadOnChange: true)
+            .Build();
 
         var telegramConfig = Configuration.GetSection("Telegram").Get<TelegramConfig>();
 
-        if(telegramConfig == null)
+        if (telegramConfig == null)
         {
-            Console.WriteLine("Telegram configuration was not loaded"); 
+            Console.WriteLine("Telegram configuration was not loaded");
             return;
         }
 
